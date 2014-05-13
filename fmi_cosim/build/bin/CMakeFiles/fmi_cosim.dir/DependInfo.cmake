67d2ba5a959bc578c402f039d17b8853fac05bb0
Thiyagarajan Purusothaman <thiag.ece@gmail.com>
2014-05-13 12:54:05 +0200

    restructured for cmake build. possibly this can be imported to Xcode or eclipse... Next step is to create wrapper in C++

---
+++ fmi_cosim/build/bin/CMakeFiles/fmi_cosim.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/Users/thiag/priv-git/fmi_test/src/main_cs.c" "/Users/thiag/priv-git/fmi_test/build/bin/CMakeFiles/fmi_cosim.dir/main_cs.c.o"
+  "/Users/thiag/priv-git/fmi_test/src/sim_support.c" "/Users/thiag/priv-git/fmi_test/build/bin/CMakeFiles/fmi_cosim.dir/sim_support.c.o"
+  "/Users/thiag/priv-git/fmi_test/src/stack.c" "/Users/thiag/priv-git/fmi_test/build/bin/CMakeFiles/fmi_cosim.dir/stack.c.o"
+  "/Users/thiag/priv-git/fmi_test/src/xml_parser.c" "/Users/thiag/priv-git/fmi_test/build/bin/CMakeFiles/fmi_cosim.dir/xml_parser.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
